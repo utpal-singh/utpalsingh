4b276c6b93219652b9e249303277fec0315a6922
utpal-singh <utpal_singh@yahoo.com>
2021-12-22 23:10:33 +0530

    Some pages added and title corrected

--- pages/views.py
+++ pages/views.py
@@ -5,5 +5,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, "pages/index.html", {"title": "Home - Utpal Singh"})
 
+def about(request):
+    return render(request, "pages/about.html", {"title": "About - Utpal Singh"})
+
+def contact(request):
+    return render(request, "pages/contact.html", {"title": "Contact - Utpal Singh"})
+
+
+
+
 
 # Create your views here.
