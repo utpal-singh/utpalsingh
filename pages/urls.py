1053d536022e3ac3ae750fc995490a74f2fa6e14
utpal-singh <utpal_singh@yahoo.com>
2021-12-29 02:10:07 +0530

    verification .html edit

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name = "homepage"),
     path('about/', views.about, name = "about"),
     path('contact/', views.contact, name = "contact"),
-    path('googlea3fa796d88ba8a2b', views.google_verification, name="google_verification")
+    path('googlea3fa796d88ba8a2b.html', views.google_verification, name="google_verification")
 ]
