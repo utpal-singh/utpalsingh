34f9232d12d333f0e1e2eecb2eb01d5685e90a05
utpal-singh <utpal_singh@yahoo.com>
2021-12-31 02:10:02 +0530

    added recursion to views

--- covid19/views.py
+++ covid19/views.py
@@ -13,4 +13,30 @@ def index(request):
     # return render(request, "covid19/index.html", {"title": "Home - Utpal Singh", "results": results.json()})
     # while True:
     #     print(summary.timeh)
-    return render(request, "covid19/index.html", {"title": "Home - Utpal Singh", "date" : summary.date, "datedayyr" : summary.datedayyr, "timeh" : summary.timeh, "timem" : summary.timem, "times" : summary.times, 'globalstats' : summary.global_stats})
+    # return render(request, "covid19/index.html", {"title": "Home - Utpal Singh", "date" : summary.date, "datedayyr" : summary.datedayyr, "timeh" : summary.timeh, "timem" : summary.timem, "times" : summary.times, 'globalstats' : summary.global_stats})
+
+    while True:
+        from covid19.src.apis import covid19api
+        import requests
+        import datetime
+        import dateutil.parser
+        # import time
+
+        # apiHandler = covid19api + "/summary"
+        apiHandler = covid19api + "/summary"
+
+        rawData = requests.get(apiHandler)
+
+        dict_results = rawData.json()
+
+        date = dict_results['Date']
+
+        d = dateutil.parser.parse(date)
+        datedayyr = d.strftime('%m/%d/%Y')
+        timeh = d.strftime('%H')
+        timem = d.strftime('%M')
+        times = d.strftime('%S')
+
+        global_stats = dict_results['Global']
+        return render(request, "covid19/index.html", {"title": "Home - Utpal Singh", "date" : date, "datedayyr" : datedayyr, "timeh" : timeh, "timem" : timem, "times" : times, 'globalstats' : global_stats})
+
